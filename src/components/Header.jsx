c14de701c51ce95fcb63a6d6f1f61bdc10551bec
Абрамов Виталий Андреевич <_>
2024-02-28 20:55:06 +0400

    Begin new project and add begin components footer and header
    Clear project and add footer and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <span className="logo">Retro Punk</span>
+            </div>
+            <div className="presentation">
+                
+            </div>
+        </header>
+    );
+}
