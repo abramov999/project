1235f6b62e243ad43178bef7966ed8691018f9e7
Абрамов Виталий Андреевич <_>
2024-03-06 20:57:38 +0400

    Refactoring structure, add banner and styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import styles from "./Footer.module.scss";
+
 export default function Footer(){
     return(
         <footer>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import styles from "./Header.module.scss";
+
 export default function Header(){
     return(
         <header>
             <div>
-                <span className="logo">Retro Punk</span>
+                <span className={styles.logo}>Retro Punk</span>
             </div>
-            <div className="presentation">
-                
+            <div className={styles.presentation}>
+    
             </div>
         </header>
     );
