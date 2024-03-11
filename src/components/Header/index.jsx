237489e484b28882142262393bc3740cc5974cde
Абрамов Виталий Андреевич <_>
2024-03-11 20:59:09 +0400

    Add images and styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,11 @@ export default function Header(){
         <header>
             <div>
                 <span className={styles.logo}>Retro Punk</span>
+                <ul className={styles.nav}>
+                    <li>О нас</li>
+                    <li>Контакты</li>
+                    <li>Личный кабинет</li>
+                </ul>
             </div>
             <div className={styles.presentation}>
     
